7a3bbed8fe45cbdc4e9ccef773f4ac8843c39ad4
wangkuntian <wangkuntian@bonc.com.cn>
2021-03-30 17:14:12 +0800

    1.0

--- lessons/1.1 hello world/main.go
+++ lessons/1.1 hello world/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var i rune = 0
+	fmt.Println("Hello World", i)
 }
--- lessons/1.3 找出重复行/main.go
+++ lessons/1.3 找出重复行/main.go
@@ -21,18 +21,14 @@ func find() {
 func find2() {
 	counts := make(map[string]int)
 	files := [1]string{"1.txt"}
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "find2: %v \n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
+	for _, arg := range files {
+		f, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "find2: %v \n", err)
+			continue
 		}
+		countLines(f, counts)
+		f.Close()
 	}
 	for line, n := range counts {
 		if n > 1 {
