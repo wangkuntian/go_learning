bf0989b39e0bf541a82826a814c8df94dae3f602
wangkuntian <wangkuntian@bonc.com.cn>
2021-04-07 10:09:52 +0800

    3.0

---
+++ lessons/3 基本数据类型/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+
+	s := "Hello, 世界"
+	fmt.Println(len(s))
+	fmt.Println(utf8.RuneCountInString(s))
+
+	for i := 0; i < len(s); {
+		r, size := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%d\t%c\n", i, r)
+		i += size
+	}
+}
